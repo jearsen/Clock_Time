d30a7bd026f6eae4628e1aca269294997a34c2e2
廖毅森 <55884724+jearsen@users.noreply.github.com>
2020-08-24 20:33:49 +0800

    Create systick.h

---
+++ USER/systick/systick.h
@@ -0,0 +1,18 @@
+#ifndef __SYSTICK_H__
+#define __SYSTICK_H__
+
+#include "stm32f10x.h"
+
+
+		void delay_Init(u8 SYSCLK);
+		void delay_ms(u16 nms);
+		void delay_us(u16 nus);
+
+
+
+
+#endif  
+
+
+
+
